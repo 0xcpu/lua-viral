e69c68e544327185e2b4d659b49e28ec120066de
ner0x652 <cornel.punga@gmail.com>
2016-03-18 22:06:43 +0200

    Use optparse instead of lapp. Modified options

--- src/luaviral.lua
+++ src/luaviral.lua
@@ -1,13 +1,44 @@
-local lapp = require 'pl.lapp'
+local optionParser = require 'optparse'
+local utils        = require 'utils'
 
-local args = lapp [[
-    A VirusTotal client <version 0.1>
-    -f, --scanfile      <string>     File to scan
-    -g, --rescanfile                 Rescan an already submitted file
-    -j, --reportfile                 Retrieve file scan report
-    -u, --urlscan       <string>     URL to scan
-    -i, --reporturl                  Retrieve url scan report
-    -h, --help                       Print help
+local help = [[
+luaviral 0.1 - A client application that can interact with VirusTotal.
+
+Year: 2016
+License: The MIT License (MIT)
+    
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+Written by Cornel Punga
+Please report bugs at https://github.com/ner0x652/lua-viral
+
+Usage: luaviral [<options>] [<file> | <url>]...
+
+luaviral - VirusTotal client. 
+
+Options:
+
+      -h, --help               display this help, then exit
+          --version            display version information, then exit
+      -v, --verbose            a combined short and long option
+      -f, --file=FILE          Send FILE for scanning
+      -s, --file-rescan        Send FILE for rescanning
+      -r, --file-report        Retrieve file scan reports
+      -u, --url=URL            Send URL for scanning
+      -i, --url-report         Retrieve url scan reports
+      -o, --output=[FILE]      File to write output
+
 ]]
 
+local function luaviral()
+   local parser = optionParser(help)
+   local arg, opts = parser:parse(_G.arg)
+end
 
+luaviral()
